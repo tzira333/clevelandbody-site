22d6051ed55530455a04507b523117219194980c
tzira333 <ttzirallc@gmail.com>
2026-01-23 01:54:41 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: { extend: {} },
-  plugins: [],
-},
   theme: {
     extend: {
       colors: {
